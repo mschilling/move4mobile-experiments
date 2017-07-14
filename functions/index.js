dac98a474ef1fedee1c61ed90be7383d0f759318
Michael Schilling <mschilling@arkid.nl>
2017-07-14 11:39:56 +0200

    Initial repo commit

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+// We DO NOT manually modify this file. It's handled by Webpack
+
+'use strict';
+
+const functions = require('firebase-functions');
---
+++ functions/src/echo-service/index.ts
@@ -0,0 +1,16 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+export const listener = functions.https.onRequest(async (req, res) => {
+
+    let payload = { 
+        method: req.method,
+        headers: req.headers, 
+        body: req.body 
+    }
+    
+    const snapshot = await admin.database().ref('/echo-service').push(payload)
+    let responseText = req.body.text ? req.body.text : 'You got it!';
+    res.send({ text: responseText})
+    // res.redirect(303, snapshot.ref)
+})
